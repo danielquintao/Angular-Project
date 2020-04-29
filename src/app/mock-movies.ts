df1590b99a272c569e227bcdf846654edf1784b0
Daniel Q. M <danielquintaodemoraes@gmail.com>
2020-04-29 03:43:57 +0200

    layout quase completo

--- src/app/mock-movies.ts
+++
@@ -1,8 +0,0 @@
-import { Movie } from './movie';
-
-export const MOVIES: Movie[] = [
-  { id: 1, name: 'Les Enfants du Paradis' },
-  { id: 2, name: 'Les 40 Coups' },
-  { id: 3, name: 'Bacurau'},
-  {id: 4, name: 'Gabriela Cravo e Canela'}
-];
